d300b88fd2c231ba8dddeb4f8fddf70b32ce6562
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2015-02-09 16:29:25 +0100

    new class to pass events from control to observing view

---
+++ src/org/kontalk/misc/ViewEvent.java
@@ -0,0 +1,54 @@
+/*
+ *  Kontalk Java client
+ *  Copyright (C) 2014 Kontalk Devteam <devteam@kontalk.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kontalk.misc;
+
+import org.kontalk.model.KonMessage;
+
+/**
+ * Events passed from controller to view.
+ * @author Alexander Bikadorov <abiku@cs.tu-berlin.de>
+ */
+public abstract class ViewEvent {
+
+    /** Application status changed. */
+    public static class StatusChanged extends ViewEvent {
+    }
+
+    /** The personal account is missing (show import wizard). */
+    public static class MissingAccount extends ViewEvent {
+    }
+
+    /** An exception was thrown somewhere. */
+    public static class Exception extends ViewEvent {
+        public final KonException exception;
+
+        public Exception(KonException exception) {
+            this.exception = exception;
+        }
+    }
+
+    /** There was a security error while de-/encrypting a message. */
+    public static class SecurityError extends ViewEvent {
+        public final KonMessage message;
+
+        public SecurityError(KonMessage message) {
+            this.message = message;
+        }
+    }
+}
